d7f1e49a935a2ace8c59c9b279fb1d4fc6c46c6a
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2025-01-09 14:45:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/expensetracker/ExpenseAdapter.java
@@ -0,0 +1,50 @@
+package com.example.expensetracker;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder> {
+
+    private List<Expense> expenseList;
+
+    public ExpenseAdapter(List<Expense> expenseList) {
+        this.expenseList = expenseList;
+    }
+
+    @NonNull
+    @Override
+    public ExpenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.expense_item, parent, false);
+        return new ExpenseViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExpenseViewHolder holder, int position) {
+        Expense expense = expenseList.get(position);
+        holder.tvCategory.setText(expense.getCategory());
+        holder.tvAmount.setText(String.valueOf(expense.getAmount()));
+        holder.tvDate.setText(expense.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return expenseList.size();
+    }
+
+    static class ExpenseViewHolder extends RecyclerView.ViewHolder {
+        TextView tvCategory, tvAmount, tvDate;
+
+        public ExpenseViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCategory = itemView.findViewById(R.id.tvCategory);
+            tvAmount = itemView.findViewById(R.id.tvAmount);
+            tvDate = itemView.findViewById(R.id.tvDate);
+        }
+    }
+}
